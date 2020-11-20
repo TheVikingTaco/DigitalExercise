663081b317054c95ac23f877a4d99667164eaf10
TheVikingTaco <panchoprensa60@hotmail.com>
2020-11-19 23:56:36 -0800

    Se prepara el entregable final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DigitalExercise';
 
+  l: number = 0;
+  t: number = 0;
+  left: string = '0px';
+  top: string = '0px';
+  container: HTMLElement;
+  logo: HTMLElement;
+
+
+  ngOnInit() {
+    this.container = document.getElementById("container");
+    this.logo = document.getElementById("logo");
+
+
+
+    this.t = this.logo.getBoundingClientRect().top - this.container.getBoundingClientRect().top;
+    this.l = this.logo.getBoundingClientRect().left - this.container.getBoundingClientRect().left;
+    this.top = this.logo.getBoundingClientRect().top - this.container.getBoundingClientRect().top + "px";
+    this.left = this.logo.getBoundingClientRect().left - this.container.getBoundingClientRect().left + "px";
+  }
+
   moveUp() {
-    console.log(document.getElementById("logo").style);
-    document.getElementById("logo").style.top = document.getElementById("logo").style.top + 10;
+    const offsetTopLogo = this.logo.getBoundingClientRect().top - this.container.getBoundingClientRect().top;
+
+    if (offsetTopLogo > 0) {
+      this.t -= 20;
+      this.top = this.t + 'px';
+    }
   }
 
   moveDown() {
+    const offsetBottomLogo = this.logo.getBoundingClientRect().bottom - this.container.getBoundingClientRect().bottom;
 
+    if (-offsetBottomLogo > 0) {
+      this.t += 20;
+      this.top = this.t + 'px';
+    }
   }
 
   moveRight() {
+    const offsetLeftLogo = this.logo.getBoundingClientRect().right - this.container.getBoundingClientRect().right;
 
+    if (-offsetLeftLogo > 0) {
+      this.l += 20;
+      this.left = this.l + 'px';
+    }
   }
 
   moveLeft() {
+    const offsetLeftLogo = this.logo.getBoundingClientRect().left - this.container.getBoundingClientRect().left;
 
+    if (offsetLeftLogo > 0) {
+      this.l -= 20;
+      this.left = this.l + 'px';
+    }
   }
 }
 
